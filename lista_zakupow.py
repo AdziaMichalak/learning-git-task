27409e7f043425e2f98b385ce073edc72ffd8d90
Magdalena Michalak <mbawolik80@gmail.com>
2022-05-02 12:54:00 +0200

    Init projekt

---
+++ lista_zakupow.py
@@ -0,0 +1,5 @@
+print("lista zakupów".capitalize())
+lista = {
+    "piekarnia".capitalize(): ["chleb".capitalize(), "bułki".capitalize(), "pączek".capitalize()],
+    "warzywniak".capitalize(): ["marchew".capitalize(), "seler".capitalize(), "rukola".capitalize()],
+}
