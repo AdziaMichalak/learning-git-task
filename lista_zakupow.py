b3e08ad7e5834615b07387614cd3c8fcc8b25bf9
Magdalena Michalak <mbawolik80@gmail.com>
2022-05-02 12:57:43 +0200

    Init projekt

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -3,3 +3,9 @@ lista = {
     "piekarnia".capitalize(): ["chleb".capitalize(), "bułki".capitalize(), "pączek".capitalize()],
     "warzywniak".capitalize(): ["marchew".capitalize(), "seler".capitalize(), "rukola".capitalize()],
 }
+for sklep, rzeczy in lista.items():
+    print("idę do %s i kupuję tam: %s".capitalize() % (sklep, rzeczy))
+   
+lista1 = lista ["Piekarnia"]
+lista2 = lista ["Warzywniak"]
+print(f"w sumie kupuję {len(lista1) + len(lista2)} produktów".capitalize())
